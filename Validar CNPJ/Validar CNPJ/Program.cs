9f1bb3ef26c89d87cfe29e8ce95d17e7d4143bb8
Jefferson Reis <78965968+jeffersonreis89@users.noreply.github.com>
2021-09-10 11:18:22 -0300

    Update Program.cs

--- Validar CNPJ/Validar CNPJ/Program.cs
+++ Validar CNPJ/Validar CNPJ/Program.cs
@@ -14,8 +14,6 @@ namespace Validar_CNPJ
             var client = new RestClient($"https://receitaws.com.br/v1/cnpj/{CNPJ}");
             Console.WriteLine("resultado da consulta na receita");
             var request = new RestRequest(Method.GET);
-            request.AddHeader("postman-token", "4ca0cc13-c7f3-42c7-b3cc-e6e24dc0045a");
-            request.AddHeader("cache-control", "no-cache");
             IRestResponse response = client.Execute(request);
 
             Console.WriteLine(response.Content);
